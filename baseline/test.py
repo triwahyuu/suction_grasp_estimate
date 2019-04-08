f5c0c7078002a25b3474563adc6484e16b7f01d5
triwahyuu <triwahyu34@gmail.com>
2019-04-08 19:00:04 +0700

    have a test

--- baseline/test.py
+++ baseline/test.py
@@ -1,31 +1,17 @@
 ## Testing playground
 
-from skimage import io
-import matplotlib.cbook
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
-import torchvision.transforms as transforms
+from torchvision import models
 
-data_path = '/home/tri/skripsi/dataset/'
-df = open(data_path + 'test-split.txt')
-data = df.read().splitlines()
+model = models.resnet101(pretrained=True)
+## remove FC layers
+new_model = torch.nn.Sequential(*(list(model.children())[:-3])) 
 
-transform = transforms.Compose([transforms.ToTensor(), \
-    transforms.Normalize((0.485,0.456,0.406), (0.229,0.224,0.225))])
+## to do: set batchnorm to fixed, solution:
+# - set affine to false, then each `requires_grad` parameters to false;
+# https://github.com/isht7/pytorch-deeplab-resnet/issues/39#issuecomment-419301286
+# - set model's mode to eval 9?)
+# https://discuss.pytorch.org/t/freeze-batchnorm-layer-lead-to-nan/8385/2
+# https://discuss.pytorch.org/t/proper-way-of-fixing-batchnorm-layers-during-training/13214/4
 
-mean = (0.485,0.456,0.406)
-std = (0.229,0.224,0.225)
-fname = '000001-1'
-to_tensor = transforms.ToTensor()
-normalize = transforms.Normalize(mean, std)
-
-color_img = to_tensor(io.imread(data_path + 'color-input/' + fname + '.png'))
-color_img_norm = normalize(color_img)
-depth = to_tensor(io.imread(data_path + 'depth-input/' + fname + '.png').astype(np.float32)) * 65536/10000
-depth = depth.clamp(0.0, 1.2)
-depth_img = torch.cat([depth, depth, depth], 0)
-depth_img_norm = normalize(depth_img)
-
-label = to_tensor(io.imread(data_path + 'label/' + fname + '.png'))
-label = torch.round(label*2 + 1)
