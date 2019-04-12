7dc3d447e26dca78952858d3287e784bcb064081
triwahyuu <triwahyu34@gmail.com>
2019-04-12 17:32:12 +0700

    some fix

--- convnet/plot_result.py
+++ convnet/plot_result.py
@@ -3,19 +3,25 @@ import matplotlib.pyplot as plt
 import os
 
 path = '/home/tri/skripsi/suction_grasp_estimate/result/'
-ave_const = 250
+ave_const = 200
 
 list_dir = [a for a in sorted(os.listdir(path)) if os.path.isdir(os.path.join(path, a))]
 y = np.array([], dtype=np.float64)
 y_val = np.array([], dtype=np.float64)
+train_loss = np.array([], dtype=np.float64)
+val_loss = np.array([], dtype=np.float64)
 for d in list_dir:
     p = os.path.join(path, d, 'log.csv')
     data = np.genfromtxt(p, delimiter=',', skip_header=1, dtype=np.float)
+
     y = np.append(y, np.array([a for a in data[:,5].astype(np.float64) if not np.isnan(a)]))
     y_val = np.append(y_val, np.array([b for b in data[:,10] if not np.isnan(b)]))
+    train_loss = np.append(train_loss, np.array([a for a in data[:,2].astype(np.float64) if not np.isnan(a)]))
+    val_loss = np.append(val_loss, np.array([a for a in data[:,7].astype(np.float64) if not np.isnan(a)]))
 
 # get average
 y_ave = np.array([np.mean(y[i*ave_const:(i+1)*ave_const]) for i in range(y.shape[0]//ave_const)])
+loss_tr_ave = np.array([np.mean(train_loss[i*ave_const:(i+1)*ave_const]) for i in range(train_loss.shape[0]//ave_const)])
 x_ave = np.arange(y_ave.shape[0]).astype(np.int)
 x_val = np.arange(y_val.shape[0]).astype(np.int)
 
@@ -29,4 +35,12 @@ plt.show()
 
 plt.plot(x_val, y_val)
 plt.title('Validation Mean IU')
+plt.show()
+
+plt.plot(x_ave, loss_tr_ave)
+plt.title('Training Loss')
+plt.show()
+
+plt.plot(x_val, val_loss)
+plt.title('Validation Loss')
 plt.show()
