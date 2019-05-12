809417aa7fc13e65f3b952dc0a715c6211bf64c5
triwahyuu <triwahyu34@gmail.com>
2019-05-13 01:38:40 +0700

    cleaning up

--- baseline/predict.py
+++ baseline/predict.py
@@ -99,8 +99,13 @@ if __name__ == "__main__":
     data = df.read().splitlines()
     
     result = np.zeros((len(data), 4))
+    
     plt.ion()
     plt.show()
+    fig, ax = plt.subplots(1,3)
+    fig.subplots_adjust(top=1.0, bottom=0.0, left=0.0, right=1.0, wspace=0.0, hspace=0.0)
+    fig.canvas.set_window_title('Evaluation Result')
+
     for n,fname in tqdm.tqdm(enumerate(data), total=len(data), 
             desc='Processing', ncols=80):
         print(fname, end='\t')
@@ -138,28 +143,15 @@ if __name__ == "__main__":
 
         ## visualize
         rgb_in_np = np.asarray(rgb_in, dtype=np.uint8)
-        plt.subplot(1,3,1)
-        plt.imshow(rgb_in_np)
-        plt.yticks([]); plt.xticks([])
-        plt.subplot(1,3,2)
-        plt.imshow(score)
-        plt.yticks([]); plt.xticks([])
-        plt.subplot(1,3,3)
-        plt.imshow(label_np)
-        plt.yticks([]); plt.xticks([])
+        ax[0].imshow(rgb_in_np)
+        ax[1].imshow(score)
+        ax[2].imshow(label_np)
+        ax[0].set_axis_off()
+        ax[1].set_axis_off()
+        ax[2].set_axis_off()
+
         plt.draw()
         plt.pause(0.01)
-        
-        # fig1, ax1 = plt.subplots()
-        # fig1.subplots_adjust(top=1.0, bottom=0.0, left=0.0, right=1.0)
-        # ax1.imshow(rgb_in)
-        # ax1.set_axis_off()
-
-        # fig2, ax2 = plt.subplots()
-        # fig2.subplots_adjust(top=1.0, bottom=0.0, left=0.0, right=1.0)
-        # ax2.imshow(score)
-        # ax2.set_axis_off()
-        # plt.show()
     
     ## save the result and calculate overal precision and recall
     np.savetxt('result.txt', result, fmt='%.10f')
--- convnet/infer.py
+++ convnet/infer.py
@@ -1,49 +1,27 @@
 ## inference script 
-from vis_util import post_process, visualize
+from vis_util import visualize
+from utils import post_process, prepare_input
 from model import SuctionModel18, SuctionModel50
+
 import os
 import argparse
 import time
 
 import torch
-from torchvision.transforms import ToTensor, Normalize, Resize
 import numpy as np
 from PIL import Image
 from skimage.transform import resize
-from scipy.ndimage import gaussian_filter
 
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 
-class Struct:
-    def __init__(self, **kwargs):
-        for k, v in kwargs.items():
-            setattr(self, k, v)
-        
-options = Struct(\
-    data_path = '/home/tri/skripsi/dataset/',
-    model_path = '/home/tri/skripsi/result/04_resnet18_dropout/20190429_222828/model_best.pth.tar',
-    img_height =  480,
-    img_width = 640,
-    output_scale = 8,
-    arch = 'resnet18',
-    device = 'cuda:0'
-)
-
-def prepare_input(color, depth, device):
-    to_tensor = ToTensor()
-    normalize = Normalize((0.485,0.456,0.406), (0.229,0.224,0.225))
-
-    color_img = normalize(to_tensor(color))
-    color_img = color_img.view(1, color_img.size(0), color_img.size(1), color_img.size(2))
-
-    depth = (to_tensor(np.asarray(depth, dtype=np.float32)) * 65536/10000).clamp(0.0, 1.2)
-    depth_img = torch.cat([depth, depth, depth], 0)
-    depth_img = normalize(depth_img)
-    depth_img = depth_img.view(1, depth_img.size(0), depth_img.size(1), depth_img.size(2))
-
-    img_input = [color_img.to(device), depth_img.to(device)]
-    return img_input
+class Options(object):
+    def __init__(self):
+        self.data_path = '/home/tri/skripsi/dataset/'
+        self.model_path = '/home/tri/skripsi/result/04_resnet18_dropout/20190429_222828/model_best.pth.tar'
+        self.img_height =  480
+        self.img_width = 640
+        self.output_scale = 8
+        self.arch = 'resnet18'
+        self.device = 'cuda:0'
 
 if __name__ == "__main__":
     model_choices = ['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152']
@@ -56,18 +34,15 @@ if __name__ == "__main__":
         '--datapath', dest='data_path', default='', help='suction grasp dataset path',
     )
     parser.add_argument(
-        '--checkpoint', default='', help='suction grasp dataset path',
+        '--checkpoint', default='', help='model path',
     )
     args = parser.parse_args()
-    # np.random.seed(time.time())
+    np.random.seed(int(time.time()))
 
+    options = Options()
     options.arch = args.arch if args.arch != '' else options.arch
     options.data_path = args.data_path if args.data_path != '' else options.data_path
     options.model_path = args.checkpoint if args.checkpoint != '' else options.model_path
-
-    ## transforms
-    to_tensor = ToTensor()
-    normalize = Normalize((0.485,0.456,0.406), (0.229,0.224,0.225))
 
     ## get model
     if args.arch == 'resnet18' or args.arch == 'resnet34':
@@ -83,9 +58,7 @@ if __name__ == "__main__":
     ## get random image input
     sample_list = open(os.path.join(options.data_path, 'test-split.txt')).read().splitlines()
     input_file = np.random.choice(sample_list, 1)[0]
-    # color_img = to_tensor(Image.open(os.path.join(options.data_path, 'test', 'test-image.color.png')))
     color = Image.open(os.path.join(options.data_path, 'color-input', input_file + '.png'))
-    # depth = Image.open(os.path.join(options.data_path, 'test', 'test-image.depth.png'))
     depth = Image.open(os.path.join(options.data_path, 'depth-input', input_file + '.png'))
 
     img_input = prepare_input(color, depth, options.device)
@@ -94,8 +67,7 @@ if __name__ == "__main__":
     print('computing forward pass: ', input_file)
     output = model(img_input)
 
-    cls_pred = np.squeeze(output.data.max(1)[1].cpu().numpy(), axis=0)
-    cls_pred = cls_pred.astype(np.float64)
+    cls_pred = np.squeeze(output.data.max(1)[1].cpu().numpy(), axis=0).astype(np.float64)
     cls_pred = resize(cls_pred, (options.img_height, options.img_width), 
         anti_aliasing=True, mode='reflect')
 
@@ -103,8 +75,6 @@ if __name__ == "__main__":
     pred = resize(pred, (options.img_height, options.img_width), 
         anti_aliasing=True, mode='reflect')
     affordance = (pred - pred.min()) / (pred.max() - pred.min())
-    # affordance[affordance >= 1] = 0.9999 # normalize
-    # affordance[affordance < 0] = 0
 
     ## visualize
     print('post process...')
