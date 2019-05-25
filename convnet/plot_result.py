080429dbc7b784cd9d254bfea4857c6a08bab606
triwahyuu <triwahyu34@gmail.com>
2019-05-25 12:00:12 +0700

    simplifying plot log

--- convnet/plot_result.py
+++ convnet/plot_result.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     file_path = os.path.dirname(os.path.abspath(__file__))
     result_path = os.path.join('/'.join(file_path.split('/')[:-1]), 'result')
     result_path = args.result_path if args.result_path else result_path
-    ave_const = 200
+    ave_const = 735 ## average each epoch
 
     list_dir = [a for a in sorted(os.listdir(result_path))[:-1] if os.path.isdir(os.path.join(result_path, a))]
     y = np.array([], dtype=np.float64)
@@ -36,22 +36,27 @@ if __name__ == "__main__":
     x_ave = np.arange(y_ave.shape[0]).astype(np.int)
     x_val = np.arange(y_val.shape[0]).astype(np.int)
 
+    loss_max = loss_tr_ave.max() if loss_tr_ave.max() > val_loss.max() else val_loss.max()
+    loss_min = loss_tr_ave.min() if loss_tr_ave.min() < val_loss.min() else val_loss.min()
+    loss_lim = (loss_min-loss_max*0.05, loss_max+loss_max*0.05)
+    iou_max = y_ave.max() if y_ave.max() > y_val.max() else y_val.max()
+    iou_min = y_ave.min() if y_ave.min() < y_val.min() else y_val.min()
+    iou_lim = (iou_min-iou_max*0.05, iou_max+iou_max*0.05)
+    
     # print(y.shape)
     # print('minimum train loss %.10f at %d idx' % (y_ave.min(), y_ave.argmin()))
     # print('minimum validation loss %.10f at %d idx' % (y_val.min(), y_val.argmin()))
 
     plt.figure(1)
-    plt.plot(x_ave, y_ave)
-    plt.title('Training Mean IU')
+    plt.plot(x_ave, y_ave, label="Training")
+    plt.plot(x_val, y_val, label="Validation")
+    plt.ylim(iou_lim)
+    plt.title('Mean IU')
+    plt.legend()
     plt.figure(2)
-    plt.plot(x_val, y_val)
-    plt.title('Validation Mean IU')
-    plt.show()
-
-    plt.figure(1)
-    plt.plot(x_ave, loss_tr_ave)
-    plt.title('Training Loss')
-    plt.figure(2)
-    plt.plot(x_val, val_loss)
-    plt.title('Validation Loss')
+    plt.plot(x_ave, loss_tr_ave, label="Training")
+    plt.plot(x_val, val_loss, label="Validation")
+    plt.ylim(loss_lim)
+    plt.title('Loss')
+    plt.legend()
     plt.show()
