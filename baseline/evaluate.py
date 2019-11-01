ba70818eb2941f1e83deac6f62f3ec286e49607a
triwahyuu <triwahyu34@gmail.com>
2019-11-01 18:07:46 +0700

    adjustments

--- baseline/evaluate.py
+++ baseline/evaluate.py
@@ -1,41 +1,115 @@
 from PIL import Image
 import numpy as np
-import os.path
+import tqdm
+
+import os.path as osp
+import argparse
+import time
+from datetime import datetime
+import shutil
+
+from predict import predict
+
+import matplotlib.cbook
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore",category=matplotlib.cbook.mplDeprecation)
+
+p = osp.dirname(osp.abspath(__file__)).split('/')
+data_path = osp.join('/'.join(p[:-2]), 'dataset/')
+result_path = osp.join('/'.join(p[:-2]), 'result/baseline/')
+sample_list = open(data_path + 'test-split.txt').read().splitlines()
+
+def evaluate(log_name, settings=[0.3, 0.02, 0.1], visualize=False, save_result=False):
+    if visualize:
+        plt.ion()
+        plt.show()
+        fig = plt.gcf()
+        fig.subplots_adjust(top=1.0, bottom=0.0, left=0.0, right=1.0, wspace=0.0, hspace=0.0)
+        fig.canvas.set_window_title('Baseline Prediction Result')
+
+    result = np.zeros((len(sample_list), 5))
+    settings = list(map(lambda x: round(x, 2), settings))
+    for n,fname in tqdm.tqdm(enumerate(sample_list), total=len(sample_list), desc='Processing', ncols=80):
+        ## load the datasets
+        rgb_in = Image.open(osp.join(data_path, "color-input/", fname + '.png'))
+        rgb_bg = Image.open(osp.join(data_path, "color-background/", fname + '.png'))
+        depth_in = Image.open(osp.join(data_path, "depth-input/", fname + '.png'))
+        depth_bg = Image.open(osp.join(data_path, "depth-background/", fname + '.png'))
+        cam_intrinsic = np.loadtxt(osp.join(data_path, "camera-intrinsics/", fname + '.txt'))
+
+        ## get the suction affordance
+        tm = time.perf_counter()
+        surf_norm, score = predict(rgb_in, rgb_bg, depth_in, depth_bg, cam_intrinsic, settings)
+        tm = time.perf_counter() - tm
+
+        if save_result:
+            score_im = Image.fromarray((score*255).astype(np.uint8))
+            score_im.save(osp.join(result_path, fname + '.png'))
+
+        ## Load ground truth manual annotations for suction affordances
+        ## 0 - negative, 128 - positive, 255 - neutral (no loss)
+        label = Image.open(data_path + 'label/' + fname + '.png')
+        label_np = np.asarray(label, dtype=np.uint8)
+
+        ## Suction affordance threshold
+        ## take the top 1% prediction
+        # threshold = score.max() - 0.0001
+        threshold = np.percentile(score, 99)
+        score_norm = (score*255).astype(np.uint8)
+        sum_tp = np.sum(np.logical_and((score > threshold), (label_np == 128)).astype(np.int))
+        sum_fp = np.sum(np.logical_and((score > threshold), (label_np == 0)).astype(np.int))
+        sum_tn = np.sum(np.logical_and((score <= threshold), (label_np == 0)).astype(np.int))
+        sum_fn = np.sum(np.logical_and((score <= threshold), (label_np == 128)).astype(np.int))
+        
+        result[n,:] = [sum_tp, sum_fp, sum_tn, sum_fn, tm]
+        
+        with open(osp.join('result/', log_name), 'a') as f:
+            precision = 0 if (sum_tp + sum_fp) == 0 else sum_tp/(sum_tp + sum_fp)
+            recall = 0 if (sum_tp + sum_fn) == 0 else sum_tp/(sum_tp + sum_fn)
+            res_str = [fname, "%.8f"%(precision), "%.8f"%(recall), sum_tp, sum_fp, sum_tn, sum_fn, "%.8f"%(tm)]
+            f.write(','.join(map(str, res_str)) + '\n')
+
+        ## visualize
+        if visualize:
+            rgb_in_np = np.asarray(rgb_in, dtype=np.uint8)
+            plt.subplot(1,3,1)
+            plt.imshow(rgb_in_np)
+            plt.yticks([]); plt.xticks([])
+            plt.subplot(1,3,2)
+            plt.imshow(score)
+            plt.yticks([]); plt.xticks([])
+            plt.subplot(1,3,3)
+            plt.imshow(label_np)
+            plt.yticks([]); plt.xticks([])
+            plt.draw()
+            plt.pause(0.01)
+    
+    return result
+
 
 if __name__ == "__main__":
-    data_path = '/home/tri/skripsi/dataset/'
-    code_path = '/home/tri/skripsi/suction_grasp_estimate/'
-    
-    df = open(data_path + 'test-split.txt')
-    data = df.read().splitlines()
-    data_metric = np.zeros((len(data), 4))
-    metrics = np.zeros((len(data), 2))
-    for n, fname in enumerate(data):
-        print(fname, end='\t')
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '--datapath', dest='data_path', default='', help='suction grasp dataset path',
+    )
+    parser.add_argument(
+        '-v','--visualize', action='store_true', help='visualize result (pls don\'t use this)'
+    )
+    parser.add_argument(
+        '-s','--save-result', action='store_true', help='save prediction result'
+    )
+    args = parser.parse_args()
 
-        ## get the baseline result and ground truth label
-        result = Image.open(data_path + 'baseline/' + fname + '.png')
-        label = Image.open(data_path + 'label/' + fname + '.png')
+    data_path = args.data_path if args.data_path != '' else data_path
+    result_log_name = 'result-' + datetime.now().strftime('%m%d_%H%M') + '.txt'
 
-        ## calculate the precision and recall
-        result_np = np.asarray(result, dtype=np.uint8)
-        label_np = np.asarray(label, dtype=np.uint8)
-        threshold = np.percentile(result_np, 99)
-        sum_tp = np.sum(np.logical_and((result_np > threshold), (label_np == 128)).astype(np.int))
-        sum_fp = np.sum(np.logical_and((result_np > threshold), (label_np == 0)).astype(np.int))
-        sum_tn = np.sum(np.logical_and((result_np <= threshold), (label_np == 0)).astype(np.int))
-        sum_fn = np.sum(np.logical_and((result_np <= threshold), (label_np == 128)).astype(np.int))
-        
-        precision = sum_tp/(sum_tp + sum_fp)
-        recall = sum_tp/(sum_tp + sum_fn)
-        data_metric[n,:] = [sum_tp, sum_fp, sum_tn, sum_fn]
-        metrics[n,:] = [precision, recall]
-        print("%.8f\t%.8f" % (precision, recall))
-        n += 1
+    result = evaluate(result_log_name, visualize=args.visualize, save_result=args.save_result)
     
-    s = data_metric.sum(axis=0)
+    ## save the result and calculate overal precision and recall
+    shutil.copy(osp.join('result/', result_log_name), osp.join(result_path, 'log/'))
+    s = result.sum(axis=0)
     precision = s[0]/(s[0]+s[1])
     recall = s[0]/(s[0]+s[3])
-    # np.vstack((metrics, np.array([precision, recall])))
-    np.savetxt(os.path.join(data_path, 'baseline','result.txt'), metrics, fmt='%.10f')
-    print(precision, recall)
+    time_ave = s[4]/result.shape[0]
+    print(precision, recall, time_ave)
--- baseline/predict.py
+++ baseline/predict.py
@@ -1,7 +1,10 @@
 from PIL import Image
 import numpy as np
 import tqdm
+
 import argparse
+import os.path as osp
+import time
 
 import open3d
 from scipy.ndimage.filters import generic_filter
@@ -29,20 +32,31 @@ def stdev_filter(im, window_size):
         res[:,:,i] = np.sqrt(c2 - c1*c1)
     return res
 
+def calc_metric(prediction, target, threshold):
+    sum_tp = np.sum(np.logical_and((prediction > threshold), (label_np == 128)).astype(np.int))
+    sum_fp = np.sum(np.logical_and((prediction > threshold), (label_np == 0)).astype(np.int))
+    sum_tn = np.sum(np.logical_and((prediction <= threshold), (label_np == 0)).astype(np.int))
+    sum_fn = np.sum(np.logical_and((prediction <= threshold), (label_np == 128)).astype(np.int))
+    return [sum_tp, sum_fp, sum_tn, sum_fn]
+
 ## predict the affordance map and surface normal map
 ## take the inputs of PIL image and the camera intrinsic matrix in numpy
-def predict(color_input, color_bg, depth_input, depth_bg, camera_intrinsic):
+## settings: [color_frg_threshold, depth_frg_threshold, normal_radius]
+def predict(color_input, color_bg, depth_input, depth_bg, camera_intrinsic, settings=[0.3, 0.02, 0.1]):
+    # print(time.time())
     ## scale the images to the proper value
     color_input = np.asarray(color_input, dtype=np.float32) / 255
     color_bg = np.asarray(color_bg, dtype=np.float32) / 255
     depth_input = np.asarray(depth_input, dtype=np.float64) / 10000
     depth_bg = np.asarray(depth_bg, dtype=np.float64) / 10000
 
+    # print(time.time())
     ## get foreground mask
-    frg_mask_color = ~(np.sum(abs(color_input-color_bg) < 0.3, axis=2) == 3)
-    frg_mask_depth = np.logical_and((abs(depth_input-depth_bg) > 0.02), (depth_bg != 0))
+    frg_mask_color = ~(np.sum(abs(color_input-color_bg) < settings[0], axis=2) == 3)
+    frg_mask_depth = np.logical_and((abs(depth_input-depth_bg) > settings[1]), (depth_bg != 0))
     foreground_mask = np.logical_or(frg_mask_color, frg_mask_depth)
 
+    # print(time.time())
     ## project depth to camera space
     pix_x, pix_y = np.meshgrid(np.arange(640), np.arange(480))
     cam_x = (pix_x - camera_intrinsic[0][2]) * depth_input/camera_intrinsic[0][0]
@@ -52,33 +66,27 @@ def predict(color_input, color_bg, depth_input, depth_bg, camera_intrinsic):
     depth_valid = (np.logical_and(foreground_mask, cam_z) != 0)
     input_points = np.array([cam_x[depth_valid], cam_y[depth_valid], cam_z[depth_valid]]).transpose()
 
+    # print(time.time())
     ## get the foreground point cloud
     pcd = open3d.PointCloud()
     pcd.points = open3d.Vector3dVector(input_points)
-    open3d.estimate_normals(pcd, search_param=open3d.KDTreeSearchParamHybrid(radius = 0.1, max_nn = 50))
+    open3d.estimate_normals(pcd, search_param=open3d.KDTreeSearchParamHybrid(radius=settings[2], max_nn=100))
+    
+    # print(time.time())
+    ## flip normals to point towards sensor
+    open3d.geometry.orient_normals_towards_camera_location(pcd, [0,0,0])
     pcd_normals = np.asarray(pcd.normals)
 
-    ## flip normals to point towards sensor
-    center = [0,0,0]
-    for k in range(input_points.shape[0]):
-        p1 = center - input_points[k][:]
-        p2 = pcd_normals[k][:]
-        x = np.cross(p1,p2)
-        angle = np.arctan2(np.sqrt((x*x).sum()), p1.dot(p2.transpose()))
-        if (angle > -np.pi/2 and angle < np.pi/2):
-            pcd_normals[k][:] = -pcd_normals[k][:]
-
+    # print(time.time())
     ## reproject the normals back to image plane
     pix_x = np.round((input_points[:,0] * camera_intrinsic[0][0] / input_points[:,2] + camera_intrinsic[0][2]))
-    pix_y = np.round((input_points[:,1] * camera_intrinsic[1][1] / input_points[:,2] + cam_intrinsic[1][2]))
+    pix_y = np.round((input_points[:,1] * camera_intrinsic[1][1] / input_points[:,2] + camera_intrinsic[1][2]))
 
     surface_normals_map = np.zeros(color_input.shape)
     for n, (x,y) in enumerate(zip(pix_x, pix_y)):
-        x,y = int(x), int(y)
-        surface_normals_map[y,x,0] = pcd_normals[n,0]
-        surface_normals_map[y,x,1] = pcd_normals[n,1]
-        surface_normals_map[y,x,2] = pcd_normals[n,2]
-        
+        surface_normals_map[int(y),int(x)] = pcd_normals[n]
+    
+    # print(time.time())
     ## Compute standard deviation of local normals
     mean_std_norms = np.mean(stdev_filter(surface_normals_map, 25), axis=2)
     affordance_map = 1 - mean_std_norms/mean_std_norms.max()
@@ -91,76 +99,62 @@ if __name__ == "__main__":
     parser.add_argument(
         '--datapath', dest='data_path', default='', help='suction grasp dataset path',
     )
+    parser.add_argument(
+        '--img-input', default='', help='input image index, eg: 00001-1',
+    )
     args = parser.parse_args()
+    np.random.seed(int(time.time()))
 
-    data_path = '/home/tri/skripsi/dataset/'
-    data_path = args.data_path if args.data_path != '' else data_path
-    df = open(data_path + 'test-split.txt')
-    data = df.read().splitlines()
+    p = osp.dirname(osp.abspath(__file__)).split('/')
+    data_path = args.data_path if args.data_path != '' else osp.join('/'.join(p[:-2]), 'dataset/')
+    result_path = osp.join('/'.join(p[:-2]), 'result/baseline/')
+
+    sample_list = open(osp.join(data_path, 'test-split.txt')).read().splitlines()
+    input_file = np.random.choice(sample_list, 1)[0] if args.img_input == '' else args.img_input
+
+    ## load the datasets
+    rgb_in = Image.open(data_path + 'color-input/' + input_file + '.png')
+    rgb_bg = Image.open(data_path + "/color-background/" + input_file + ".png")
+    depth_in = Image.open(data_path + "/depth-input/" + input_file + ".png")
+    depth_bg = Image.open(data_path + "/depth-background/" + input_file + ".png")
+    cam_intrinsic = np.loadtxt(data_path + 'camera-intrinsics/' + input_file + '.txt')
+
+    ## get the suction affordance
+    surf_norm, score = predict(rgb_in, rgb_bg, depth_in, depth_bg, cam_intrinsic)
+    score_im = Image.fromarray((score*255).astype(np.uint8))
+    score_im.save(osp.join(result_path, input_file + '.png'))
+
+    ## Load ground truth manual annotations for suction affordances
+    ## 0 - negative, 128 - positive, 255 - neutral (no loss)
+    label = Image.open(data_path + 'label/' + input_file + '.png')
+    label_np = np.asarray(label, dtype=np.uint8)
+
+    ## Suction affordance threshold
+    ## take the top 1% prediction
+    # threshold = score.max() - 0.0001
+    threshold = np.percentile(score, 99)
+    score_norm = (score*255).astype(np.uint8)
+    sum_tp = np.sum(np.logical_and((score > threshold), (label_np == 128)).astype(np.int))
+    sum_fp = np.sum(np.logical_and((score > threshold), (label_np == 0)).astype(np.int))
+    sum_tn = np.sum(np.logical_and((score <= threshold), (label_np == 0)).astype(np.int))
+    sum_fn = np.sum(np.logical_and((score <= threshold), (label_np == 128)).astype(np.int))
     
-    result = np.zeros((len(data), 4))
-    
-    plt.ion()
-    plt.show()
+    precision = sum_tp/(sum_tp + sum_fp) if (sum_tp + sum_fp) != 0 else 0
+    recall = sum_tp/(sum_tp + sum_fn) if (sum_tp + sum_fp) != 0 else 0
+    print("%s\t%.8f\t%.8f" % (input_file, precision, recall))
+
+    ## visualize
+    rgb_in_np = np.asarray(rgb_in, dtype=np.uint8)
+    plt.subplot(1,3,1)
+    plt.imshow(rgb_in_np)
+    plt.yticks([]); plt.xticks([])
+    plt.subplot(1,3,2)
+    plt.imshow(score)
+    plt.yticks([]); plt.xticks([])
+    plt.subplot(1,3,3)
+    plt.imshow(label_np)
+    plt.yticks([]); plt.xticks([])
     fig = plt.gcf()
     fig.subplots_adjust(top=1.0, bottom=0.0, left=0.0, right=1.0, wspace=0.0, hspace=0.0)
     fig.canvas.set_window_title('Baseline Prediction Result')
-
-    for n,fname in tqdm.tqdm(enumerate(data), total=len(data), 
-            desc='Processing', ncols=80):
-        print(fname, end='\t')
-
-        ## load the datasets
-        rgb_in = Image.open(data_path + 'color-input/' + fname + '.png')
-        rgb_bg = Image.open(data_path + "/color-background/" + fname + ".png")
-        depth_in = Image.open(data_path + "/depth-input/" + fname + ".png")
-        depth_bg = Image.open(data_path + "/depth-background/" + fname + ".png")
-        cam_intrinsic = np.loadtxt(data_path + 'camera-intrinsics/' + fname + '.txt')
-
-        ## get the suction affordance
-        surf_norm, score = predict(rgb_in, rgb_bg, depth_in, depth_bg, cam_intrinsic)
-        score_im = Image.fromarray((score*255).astype(np.uint8))
-        score_im.save(data_path + 'baseline/' + fname + '.png')
-
-        ## Load ground truth manual annotations for suction affordances
-        ## 0 - negative, 128 - positive, 255 - neutral (no loss)
-        label = Image.open(data_path + 'label/' + fname + '.png')
-        label_np = np.asarray(label, dtype=np.uint8)
-
-        ## Suction affordance threshold
-        ## take the top 1% prediction
-        # threshold = score.max() - 0.0001
-        threshold = np.percentile(score, 99)
-        score_norm = (score*255).astype(np.uint8)
-        sum_tp = np.sum(np.logical_and((score > threshold), (label_np == 128)).astype(np.int))
-        sum_fp = np.sum(np.logical_and((score > threshold), (label_np == 0)).astype(np.int))
-        sum_tn = np.sum(np.logical_and((score <= threshold), (label_np == 0)).astype(np.int))
-        sum_fn = np.sum(np.logical_and((score <= threshold), (label_np == 128)).astype(np.int))
-        precision = sum_tp/(sum_tp + sum_fp)
-        recall = sum_tp/(sum_tp + sum_fn)
-        result[n,:] = [sum_tp, sum_fp, sum_tn, sum_fn]
-        print("%.8f\t%.8f" % (precision, recall))
-
-        ## visualize
-        rgb_in_np = np.asarray(rgb_in, dtype=np.uint8)
-        plt.subplot(1,3,1)
-        plt.imshow(rgb_in_np)
-        plt.yticks([]); plt.xticks([])
-        plt.subplot(1,3,2)
-        plt.imshow(score)
-        plt.yticks([]); plt.xticks([])
-        plt.subplot(1,3,3)
-        plt.imshow(label_np)
-        plt.yticks([]); plt.xticks([])
-        plt.draw()
-        plt.pause(0.01)
-
-        plt.draw()
-        plt.pause(0.01)
-    
-    ## save the result and calculate overal precision and recall
-    np.savetxt('result.txt', result, fmt='%.10f')
-    s = result.sum(axis=0)
-    precision = s[0]/(s[0]+s[1])
-    recall = s[0]/(s[0]+s[3])
-    print(precision, recall)
+    plt.show()
