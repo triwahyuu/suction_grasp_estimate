4084fd6fb2e49ce2beb6686a194b780634275fdb
triwahyuu <triwahyu34@gmail.com>
2019-12-17 13:30:38 +0700

    change train script

--- experiment/train_rest.sh
+++ experiment/train_rest.sh
@@ -1,8 +1,8 @@
-python3 train.py -a fcneffnetb1
-python3 train.py -a fcneffnetb2
-python3 train.py -a fcneffnetb3
-python3 train.py -a pspeffnetb2
-python3 train.py -a resnet34
-python3 train.py -a pspnet34
-python3 train.py -a pspnet50
-python3 train.py -a bisenet34
+# python3 train.py -a fcneffnetb1 --max-epoch 100
+python3 train.py -a fcneffnetb2 --max-epoch 100
+python3 train.py -a fcneffnetb3 --max-epoch 100
+python3 train.py -a pspeffnetb2 --max-epoch 100
+python3 train.py -a resnet34 --max-epoch 100
+python3 train.py -a pspnet34 --max-epoch 100
+python3 train.py -a pspnet50 --max-epoch 100
+python3 train.py -a bisenet34 --max-epoch 100
