5154ec75a59344f31f8a17cc644a8e0e1acb23a0
triwahyuu <triwahyu34@gmail.com>
2019-12-28 19:35:25 +0700

    changing auto train script

--- experiment/train_rest.sh
+++ experiment/train_rest.sh
@@ -2,10 +2,34 @@
 
 set -o xtrace
 
-python3 train.py -a pspeffnetb2 --max-epoch 120 --resume result/pspeffnetb2/20191219_065756/checkpoint.pth.tar
-python3 train.py -a resnet34 --max-epoch 120 --resume result/resnet34/20191219_121356/checkpoint.pth.tar
-python3 train.py -a bisenet34 --max-epoch 100
-python3 train.py -a biseeffnetb0 --max-epoch 100
-python3 train.py -a biseeffnetb1 --max-epoch 100
-python3 train.py -a biseeffnetb2 --max-epoch 100
-python3 train.py -a biseeffnetb3 --max-epoch 100
+## all models
+# resnet=$(echo resnet{18,34,50,101})
+# pspnet=$(echo pspnet{18,34,50,101})
+# bisenet=$(echo bisenet{34,18,50})
+# fcneffnet=$(echo fcneffnetb{0..4})
+# pspeffnet=$(echo pspeffnetb{0..4})
+# biseeffnet=$(echo biseeffnetb{0..4})
+
+resnet=$(echo resnet{18,50,101})
+pspnet=$(echo pspnet{18,101})
+bisenet=$(echo bisenet{34,18,50})
+fcneffnet=$(echo fcneffnetb{0,4})
+pspeffnet=$(echo pspeffnetb{0,4,1,3})
+biseeffnet=$(echo biseeffnetb{0..4})
+
+function train(){
+    for a in $@; do
+        python3 train.py -a $a --max-epoch 100
+    done
+}
+
+function train2(){
+    for a in $@; do
+        python3 train.py -a $a --max-epoch 120
+    done
+}
+
+# train $bisenet $biseeffnet $resnet $pspnet $fcneffnet $pspeffnet 
+# train $pspeffnet $resnet $pspnet $fcneffnet
+train $pspeffnet
+train2 $biseeffnet $bisenet
